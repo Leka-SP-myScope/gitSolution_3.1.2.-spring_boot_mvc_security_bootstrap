90e53057a066b4d515d70b39c8af9419f84ef596
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-05-05 14:37:42 +0300

    Commit 3 05.05.21 Working application...

--- src/main/java/com/lekasp/spring_boot/first_spring_boot_mvc_security/dao/UserDaoImpl.java
+++ src/main/java/com/lekasp/spring_boot/first_spring_boot_mvc_security/dao/UserDaoImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
@@ -19,26 +20,39 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public List<User> getAllUser() {
-        Session session = entityManager.unwrap(Session.class);
-        return session.createQuery("select u from User u", User.class).getResultList();
+//        Session session = entityManager.unwrap(Session.class);
+//        return session.createQuery("select u from User u", User.class).getResultList();
+
+        Query query = entityManager.createQuery("select u from User u");
+        return query.getResultList();
     }
 
     @Override
     public Optional<User> findById(Long id) {
-        Session session = entityManager.unwrap(Session.class);
-        TypedQuery<User> query = session.createQuery(
-                "select u from User u where u.id = :id", User.class);
-        if (id != null) {
+//        Session session = entityManager.unwrap(Session.class);
+//        TypedQuery<User> query = session.createQuery(
+//                "select u from User u where u.id = :id", User.class);
+//        if (id != null) {
+//            query.setParameter("id", id);
+//            return Optional.ofNullable(query.getSingleResult());
+//        }
+//        throw new NoResultException("No User by: " + id + " present");
+
+        Query query = entityManager.createQuery(
+                "select u from User u where u.id = :id");
+        if (query != null) {
             query.setParameter("id", id);
-            return Optional.ofNullable(query.getSingleResult());
+            return Optional.ofNullable((User) query.getSingleResult());
         }
         throw new NoResultException("No User by: " + id + " present");
     }
 
     @Override
     public void saveUser(User user) {
-        Session session = entityManager.unwrap(Session.class);
-        session.saveOrUpdate(user);
+//        Session session = entityManager.unwrap(Session.class);
+//        session.saveOrUpdate(user);
+
+        entityManager.merge(user);
     }
 
 //    @Override
@@ -49,12 +63,17 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void deleteById(Long id) {
-        Session session = entityManager.unwrap(Session.class);
-        TypedQuery<User> query = session.createQuery(
-                "select u from User u where u.id = :id", User.class);
+//        Session session = entityManager.unwrap(Session.class);
+//        TypedQuery<User> query = session.createQuery(
+//                "select u from User u where u.id = :id", User.class);
+//        query.setParameter("id", id);
+//        User user = query.getResultList().stream().findAny().orElse(null);
+//        session.remove(user);
+
+        Query query = entityManager.createQuery(
+                "select u from User u where u.id = :id");
         query.setParameter("id", id);
-        User user = query.getResultList().stream().findAny().orElse(null);
-        session.remove(user);
+        entityManager.remove(query.getSingleResult());
     }
 
     @Override
