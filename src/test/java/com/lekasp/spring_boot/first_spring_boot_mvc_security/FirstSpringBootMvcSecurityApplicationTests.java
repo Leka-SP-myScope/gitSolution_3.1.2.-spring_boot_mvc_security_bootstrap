33f6412c92d79a98110a9028ffacc2d7bb00988a
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-05-05 11:13:29 +0300

    Commit 1 The begining...

---
+++ src/test/java/com/lekasp/spring_boot/first_spring_boot_mvc_security/FirstSpringBootMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekasp.spring_boot.first_spring_boot_mvc_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootMvcSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
