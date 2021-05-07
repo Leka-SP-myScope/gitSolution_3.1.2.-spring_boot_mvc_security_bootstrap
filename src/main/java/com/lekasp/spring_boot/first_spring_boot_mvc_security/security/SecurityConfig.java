0a5395ad8a404018f2f5b2f70d61b86c9ed37b25
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-05-07 13:45:37 +0300

    Commit 2 07.05.21 Working application with BCryptPasswordEncoder

--- src/main/java/com/lekasp/spring_boot/first_spring_boot_mvc_security/security/SecurityConfig.java
+++ src/main/java/com/lekasp/spring_boot/first_spring_boot_mvc_security/security/SecurityConfig.java
@@ -2,14 +2,12 @@ package com.lekasp.spring_boot.first_spring_boot_mvc_security.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity
@@ -46,13 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login");
     }
 
-//    @Bean
-//    protected BCryptPasswordEncoder passwordEncoder() {
-//        return new BCryptPasswordEncoder(12);
-//    }
-
     @Bean
-    protected NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    protected BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
     }
 }
