acb8d6ae029d6df919c934e8c8e87547959445cd
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-06-18 13:26:47 +0300

    Commit 3 18.06.21 Add RoleDto.java.

---
+++ src/main/java/com/lekasp/spring_boot/spring_boot_mvc_security_bootstrap/dto/RoleDto.java
@@ -0,0 +1,38 @@
+package com.lekasp.spring_boot.spring_boot_mvc_security_bootstrap.dto;
+
+public class RoleDto {
+    private Long id;
+    private String roleName;
+
+    public RoleDto() {
+    }
+
+    public RoleDto(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleDto{" +
+                "id=" + id +
+                ", roleName='" + roleName + '\'' +
+                '}';
+    }
+}
