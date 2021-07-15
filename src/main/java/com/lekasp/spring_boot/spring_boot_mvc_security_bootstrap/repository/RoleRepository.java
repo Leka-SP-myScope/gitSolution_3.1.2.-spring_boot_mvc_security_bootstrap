eca5e5a14123a8c593e8c2525292a590b985a32a
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-07-15 22:58:46 +0300

    Commit 2 15.07.21 Change 3 files.

--- src/main/java/com/lekasp/spring_boot/spring_boot_mvc_security_bootstrap/repository/RoleRepository.java
+++ src/main/java/com/lekasp/spring_boot/spring_boot_mvc_security_bootstrap/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.lekasp.spring_boot.spring_boot_mvc_security_bootstrap.repository;
 
 import com.lekasp.spring_boot.spring_boot_mvc_security_bootstrap.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
