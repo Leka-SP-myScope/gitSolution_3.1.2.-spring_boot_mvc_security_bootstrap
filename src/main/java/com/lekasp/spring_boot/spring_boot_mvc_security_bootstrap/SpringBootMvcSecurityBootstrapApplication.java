3b61d1a60f54d9d5da9177795d537d9a9b5fa885
Leka-SP <51834191+LekaSP@users.noreply.github.com>
2021-08-02 10:19:28 +0300

    Commit 3 02.08.21 Change 1 files, working application.

--- src/main/java/com/lekasp/spring_boot/spring_boot_mvc_security_bootstrap/SpringBootMvcSecurityBootstrapApplication.java
+++ src/main/java/com/lekasp/spring_boot/spring_boot_mvc_security_bootstrap/SpringBootMvcSecurityBootstrapApplication.java
@@ -1,8 +1,6 @@
 package com.lekasp.spring_boot.spring_boot_mvc_security_bootstrap;
 
-import com.lekasp.spring_boot.spring_boot_mvc_security_bootstrap.service.UserService;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
